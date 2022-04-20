534e3b93214377ddd70bda15169266fee04dd8e8
Lucas Duarte <lucas.duartebarbosa@outlook.com>
2022-04-20 17:41:17 -0300

    algumas telas desktop feitas

---
+++ scripts.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*criando quiz*/
+let criar_quiz = {};
+let questions_array = [];
+let level_array = [];
+
+/*funções*/
+
+function criarQuiz() {
+    
+    const promise = axios.post(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes`, {
+    title: criar_quiz.title,
+    image: criar_quiz.image,
+    questions: criar_quiz.questions,
+    levels: criar_quiz.levels
+      });
+    
+      promise.then((response) => {
+        const quizz = response.data;
+        })
+    
+    }
+
+    criarQuiz

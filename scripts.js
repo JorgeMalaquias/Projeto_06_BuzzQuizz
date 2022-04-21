10ee52743c13e9f6673aa71155232873fc3f3b94
Lucas Duarte <lucas.duartebarbosa@outlook.com>
2022-04-21 11:45:33 -0300

    começando api

--- scripts.js
+++ scripts.js
@@ -1,37 +1,23 @@
+let titulo;
+let imagem;
+let pergunta;
+let nivel;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*criando quiz*/
-let criar_quiz = {};
-let questions_array = [];
-let level_array = [];
+let quizzes = []
+const divtodosQuizzes = document.que
 
 /*funções*/
 
-function criarQuiz() {
-    
-    const promise = axios.post(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes`, {
-    title: criar_quiz.title,
-    image: criar_quiz.image,
-    questions: criar_quiz.questions,
-    levels: criar_quiz.levels
-      });
-    
-      promise.then((response) => {
-        const quizz = response.data;
-        })
-    
-    }
+function criarQuizpegarListaQuizes() {
+    const promise = axios.post(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes`)
+ promise.then(carregarQuizz)
+}
 
-    criarQuiz
+function carregarQuizz(response){
+  quizzes = response.data;
+  renderizarQuizzes
+}
+
+
+criarQuizpegarListaQuizes()
+

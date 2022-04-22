429bd4fc27c08bf4c6b30f9ee9a63c976309cda3
Jorge <fulano@email.com>
2022-04-22 03:36:39 -0300

    logica de exibicao inputs niveis finalizada

--- scripts.js
+++ scripts.js
@@ -60,12 +60,12 @@ function criarQuiz() {
 
 function inserePerguntas(numeroPerguntas) {
   const conteudoDaPagina = document.querySelector(".forms-de-perguntas");
-  for(let i=0;i<numeroPerguntas;i++){
+  for (let i = 0; i < numeroPerguntas; i++) {
     conteudoDaPagina.innerHTML += `
 <div class="criar-item-container">
   <div class="inputs-boxes-container">
       <div class="selecionar-item">
-          <h3>Pergunta ${i+1}</h3>
+          <h3>Pergunta ${i + 1}</h3>
           <ion-icon name="create-outline" onclick="mostrarInputs(this)"></ion-icon>
       </div>
       <div class="inputs-boxes hidden">
@@ -101,23 +101,60 @@ function inserePerguntas(numeroPerguntas) {
 }
 
 let perguntaSelecionada;
-function mostrarInputs(elemento){
-  if(perguntaSelecionada!==undefined){
+function mostrarInputs(elemento) {
+  if (perguntaSelecionada !== undefined) {
     ocultarInputs();
   }
   elemento.classList.add("invisible");
   perguntaSelecionada = elemento.parentNode.parentNode.parentNode;
   let hiddenElements = elemento.parentNode.parentNode.parentNode.querySelectorAll(".hidden");
-  for(let i=0;i<hiddenElements.length;i++){
+  for (let i = 0; i < hiddenElements.length; i++) {
     hiddenElements[i].classList.remove("hidden");
   }
 }
-function ocultarInputs(){
+function ocultarInputs() {
   perguntaSelecionada.querySelector("ion-icon").classList.remove("invisible");
   perguntaSelecionada.querySelector(".inputs-boxes-container:nth-child(1) .inputs-boxes").classList.add("hidden");
   perguntaSelecionada.querySelector(".inputs-boxes-container:nth-child(2)").classList.add("hidden");
   perguntaSelecionada.querySelector(".inputs-boxes-container:nth-child(3)").classList.add("hidden");
 }
-inserePerguntas(3);
+insereNiveis(3);
 
-/*inserção de perguntas no html*/
+/*inserção de niveis no html*/
+function insereNiveis(numeroNiveis) {
+  const conteudoDaPagina = document.querySelector(".forms-de-niveis");
+  for (let i = 0; i < numeroNiveis; i++) {
+    conteudoDaPagina.innerHTML += `
+  <div class="criar-item-container">
+    <div class="inputs-boxes-container">
+        <div class="selecionar-item">
+            <h3>Nível ${i + 1}</h3>
+            <ion-icon name="create-outline" onclick="mostrarInputsNiveis(this)"></ion-icon>
+        </div>
+        <div class="inputs-boxes hidden">
+            <input type="text" placeholder="Título do nível">
+            <input type="text" placeholder="% de acerto mínima">
+            <input type="text" placeholder="URL da imagem do nível">
+            <input type="text" placeholder="Descrição do nível">
+        </div>
+    </div>
+  </div>
+  `
+  }
+  conteudoDaPagina.innerHTML += `<div class="enviar-infos" onclick="validarNiveis()">Finalizar Quizz</div>`
+}
+let nivelSelecionado;
+
+function mostrarInputsNiveis(elemento) {
+  if (nivelSelecionado !== undefined) {
+    ocultarInputsNiveis();
+  }
+  elemento.classList.add("invisible");
+  nivelSelecionado = elemento.parentNode.parentNode.parentNode;
+  let hiddenElement = elemento.parentNode.parentNode.parentNode.querySelector(".hidden");
+  hiddenElement.classList.remove("hidden");
+}
+function ocultarInputsNiveis(){
+  nivelSelecionado.querySelector("ion-icon").classList.remove("invisible");
+  nivelSelecionado.querySelector(".inputs-boxes").classList.add("hidden");
+}
